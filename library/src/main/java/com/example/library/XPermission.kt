9b7f83cc1eade0b288353509d35473b7dcf7b637
郑宜衡 <18518158917@163.com>
2020-05-22 12:35:33 +0800

    修复fragment not to attach activity问题

--- library/src/main/java/com/example/library/XPermission.kt
+++ library/src/main/java/com/example/library/XPermission.kt
@@ -16,6 +16,7 @@ object XPermission {
         } else {
             val invisibleFragment = InvisibleFragment()
             fragmentManager.beginTransaction().add(invisibleFragment, TAG)
+            fragmentManager.beginTransaction().commitNow()
             invisibleFragment
         }
         fragment.requestNow(callBack, *permissions)
